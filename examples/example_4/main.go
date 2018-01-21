85cb2870c90a7178140dfdffa3d744998c1e5526
Richard Zetterberg <richard.zetterberg@gmail.com>
2018-01-21 13:40:23 +0100

    Fixes #4 - adds integration test for all cmds

---
+++ examples/example_4/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/rzetterberg/elmobd"
+)
+
+func main() {
+	serialPath := flag.String(
+		"serial",
+		"/dev/ttyUSB0",
+		"Path to the serial device to use",
+	)
+
+	flag.Parse()
+
+	dev, err := elmobd.NewDevice(*serialPath, false)
+
+	if err != nil {
+		fmt.Println("Failed to create new device", err)
+		return
+	}
+
+	supported, err := dev.CheckSupportedCommands()
+
+	if err != nil {
+		fmt.Println("Failed to check supported commands", err)
+		return
+	}
+
+	allCommands := elmobd.GetSensorCommands()
+	carCommands := supported.FilterSupported(allCommands)
+
+	fmt.Printf("%d of %d commands supported:\n", carCommands, allCommands)
+
+	for _, cmd := range carCommands {
+		fmt.Printf("- %s supported\n", cmd.Key())
+	}
+}
